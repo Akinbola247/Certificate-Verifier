37a5c5d035ad188fa2fb3c0ec65b4fb090462f01
akinbola247 <akinbolakehinde247@gmail.com>
2023-08-16 11:02:40 +0100

    world fix

--- packages/react-app/pages/index.tsx
+++ packages/react-app/pages/index.tsx
@@ -19,8 +19,8 @@ export default function Home() {
       <div className='sm:hidden'>
         <Title />
       </div>
-      <div  className=" xxl:hidden sm:w-[400px] sm:text-center sm:mx-auto xl:hidden">
-          <h1 className="sm:text-[30px] sm:font-[900] satoshi sm:w-[370px] text-[#EEEEF0]">Verify <span className='text-[#B21888]'>Certifications</span>  in seconds</h1>
+      <div  className=" xxl:hidden sm:w-[330px] sm:text-center xl:hidden">
+          <h1 className="sm:text-[30px] sm:font-[900] satoshi sm:w-[330px] text-[#EEEEF0]">Verify <span className='text-[#B21888]'>Certifications</span>  in seconds</h1>
           <p className="sm:w-[300px] sm:font-[400] satoshi text-[#EEEEF0]">Decentralized and secure solution for issuing verifiable certificates to learners and professionals.</p>
         </div>
         <div className="xxl:hidden xl:hidden moving-div satoshi sm:mt-[20px]">For full functionality, please use a desktop browser with Metamask installed</div>
@@ -28,7 +28,7 @@ export default function Home() {
         <div className='relative z-50'>
         <InputTab />
         </div>
-          <div className='absolute top-[-40px] xl:absolute  xxl:left-[15%] xl:right-10 xl:left-10 sm:hidden'>
+          <div className='absolute top-[-40px] xl:absolute  xxl:left-[15%] xxl:right-[15%] xl:right-10 xl:left-10 sm:hidden'>
             <Image src={world} alt='world' className='xl:w-[1000px]' />
             </div>
          <Creataccount/>
