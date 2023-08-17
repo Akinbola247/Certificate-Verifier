4ec21df3480c67c28b968b53a1453209d0e9168a
akinbola247 <akinbolakehinde247@gmail.com>
2023-08-17 17:36:12 +0100

    screen fix

--- packages/react-app/pages/index.tsx
+++ packages/react-app/pages/index.tsx
@@ -31,16 +31,6 @@ export default function Home() {
         <div className='absolute top-[-2%] left-[4%] w-full xl:w-[1100px] xl:left-10  right-20 sm:hidden'>
             <Image src={world} alt='world' className='w-full' />
           </div>
-          <div className='sm:hidden absolute top-[27%]'>
-          <Box
-              w="100px"
-              h="100px"
-              style={{background : "conic-gradient(from 180deg at 50% 50%, #C729B9 -28.32deg, #B52BBA 4.67deg, #A12CBC 23.65deg, #8C2EBE 44.86deg, #792FBF 72.46deg, #6C30C0 82.5deg, #4B32C3 127.99deg, #5831C2 160.97deg, #6330C1 178.46deg, #742FC0 189.48deg, #8D2DBE 202.95deg, #A62CBC 230.66deg, #B92ABA 251.35deg, #D029B8 276.44deg, #EC27B6 306.45deg, #C729B9 331.68deg, #B52BBA 364.67deg)", boxShadow:"0px 0px 60px 0px #EC27B699"}}
-              borderRadius="50%"
-              animation="bounce 2s infinite"
-              ml="950px"
-            />
-          </div>
           <div className='sm:hidden absolute top-[10%]'>
           <Box
               w="100px"
